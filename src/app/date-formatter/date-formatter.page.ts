0eb60b2010d9522c61baa5272f243ea536e91181
Aarya Devnani <aarya_devnani@hotmail.com>
2020-07-29 17:58:28 +0530

    date changes automatically

--- src/app/date-formatter/date-formatter.page.ts
+++ src/app/date-formatter/date-formatter.page.ts
@@ -13,6 +13,7 @@ export class DateFormatterPage implements OnInit {
   month: any = "";
   year: any = "";
   date: any = "";
+  len: any = "";
   showResult: boolean = false;
   constructor(private router: Router) {}
 
@@ -26,9 +27,28 @@ export class DateFormatterPage implements OnInit {
     this.year = this.unformattedDate.toString().slice(4);
     console.log(this.year);
     this.date = this.day + "-" + this.month + "-" + this.year;
+    this.unformattedDate = this.date;
     this.showResult = true;
   }
   imageCropper() {
     this.router.navigate(["./home"]);
+  }
+  autoFormat() {
+    this.len = this.unformattedDate.toString().length;
+    if (this.len != 8) {
+      setTimeout(() => {
+        this.autoFormat();
+      }, 5000);
+      console.log("enter date");
+    } else if ((this.len = 8)) {
+      this.format();
+    }
+  }
+  clear() {
+    this.len = this.unformattedDate.toString().length;
+    if (this.len == 0) {
+      this.autoFormat();
+    } else {
+    }
   }
 }
